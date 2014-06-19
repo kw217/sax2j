f7f7c2e21f571cae28b2dc7c204f83fb8fb17654
Keith Wansbrough <Keith.Wansbrough@metaswitch.com>
2014-06-19 14:16:17 +0100

    JSON extractor - first cut.

---
+++ src/org/openmobilealliance/arc/sax2j/extract/Extractor.java
@@ -0,0 +1,279 @@
+// Extractor.java
+// (C) COPYRIGHT METASWITCH NETWORKS 2014
+package org.openmobilealliance.arc.sax2j.extract;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openmobilealliance.arc.sax2j.ProgressWriter;
+
+/**
+ * Extracts XML and JSON documents from text files saved from Microsoft Word
+ * in UTF-8 format.
+ */
+public class Extractor
+{
+  /**
+   * Beginning of a JSON section (in appendix D). Assumes particular
+   * formatting and numbering.
+   */
+  private static Pattern JSON_SECTION =
+    Pattern.compile("^(D\\.\\d+) (.*) \\(section (6\\.[\\d\\.]+)\\)$");
+
+  /**
+   * Beginning of a request or response. Group 1 matches requests, group 2
+   * matches responses, group 3 matches errors.
+   */
+  private static Pattern HTTP_BEGIN =
+    Pattern.compile("^(?:([A-Z]{3,}) /.* HTTP/\\d\\.\\d|HTTP/\\d\\.\\d (\\d{3})|([A-Z1-9]\\d*\\.\\d+) ).*$");
+
+  /**
+   * Matches start of body. If group 1 matches, it's succeeded; otherwise we've
+   * matched something we shouldn't see before the body.
+   */
+  private static Pattern START_BODY =
+    Pattern.compile("^(?:()[<{]|[A-Z]{3,} /|HTTP/|[A-Z1-9]\\d*\\.\\d+ ).*$");
+
+  /**
+   * Matches end of body. If group 1 matches, it's succeeded; otherwise we've
+   * matched something we shouldn't see in the body.
+   */
+  private static Pattern END_BODY =
+      Pattern.compile("^(</|[]}]*}|[^\\s{<]).*$");
+
+  private String mFilename;
+  private BufferedReader mReader;
+  private String mLine;
+  private ProgressWriter mProgress = new ProgressWriter.NullProgressWriter();
+
+  public Extractor(String xiFilename)
+  {
+    mFilename = xiFilename;
+  }
+
+  public void setProgressWriter(ProgressWriter xiProgress)
+  {
+    mProgress = xiProgress;
+  }
+
+  public void extract() throws IOException
+  {
+    InputStream lInput =
+        new BufferedInputStream(new FileInputStream(new File(mFilename)));
+    mReader =
+        new BufferedReader(new InputStreamReader(lInput, "UTF-8"));
+
+    findJsonSectionStart();
+  }
+
+  /**
+   * Normal, acceptable end of file.
+   * @throws IOException
+   */
+  private void endOfFile() throws IOException
+  {
+    mReader.close();
+    mReader = null;
+  }
+
+  /**
+   * Unexpected end of file
+   * @param xiMessage while doing...
+   * @throws IOException
+   */
+  private void badEndOfFile(String xiMessage) throws IOException
+  {
+    mReader.close();
+    mReader = null;
+
+    mProgress.log("*** UNEXPECTED END OF FILE while " + xiMessage);
+  }
+
+  private void findJsonSectionStart() throws IOException
+  {
+    mLine = mReader.readLine();
+
+    if (mLine != null)
+    {
+      findJsonSectionStartHere();
+      return;
+    }
+
+    endOfFile();
+  }
+
+  /**
+   * Variant that assumes the first line has already been read.
+   * @throws IOException
+   */
+  private void findJsonSectionStartHere() throws IOException
+  {
+    do {
+      Matcher lMatcher = JSON_SECTION.matcher(mLine);
+
+      if (lMatcher.matches())
+      {
+        mProgress.log("Found section JSON " + lMatcher.group(3) + ": " +
+                                  lMatcher.group(1) + " " + lMatcher.group(2));
+        findJsonBegin(lMatcher.group(3), false, false);
+        return;
+      }
+    }
+    while ((mLine = mReader.readLine()) != null);
+
+    endOfFile();
+  }
+
+  private void findJsonBegin(String xiSection,
+                             boolean xiSeenRequest,
+                             boolean xiSeenResponse) throws IOException
+  {
+    mLine = mReader.readLine();
+
+    if (mLine != null)
+    {
+      findJsonBeginHere(xiSection, xiSeenRequest, xiSeenResponse);
+      return;
+    }
+
+    badEndOfFile("searching for beginning of JSON " + xiSection);
+  }
+
+  private void findJsonBeginHere(String xiSection,
+                                 boolean xiSeenRequest,
+                                 boolean xiSeenResponse) throws IOException
+  {
+    do
+    {
+      Matcher lMatcher = HTTP_BEGIN.matcher(mLine);
+
+      if (lMatcher.matches())
+      {
+        if (lMatcher.group(3) != null)
+        {
+          if (!xiSeenRequest || !xiSeenResponse)
+          {
+            mProgress.log("*** Missing JSON " + (xiSeenRequest ? "response" : "request") + " for " + xiSection);
+          }
+
+          findJsonSectionStartHere();
+          return;
+        }
+        else
+        {
+          boolean lRequest = lMatcher.group(1) != null;
+          boolean lResponse = lMatcher.group(2) != null;
+
+          if ((lRequest && xiSeenRequest) ||
+              (lResponse && xiSeenResponse))
+          {
+            mProgress.log("*** Already seen a " +
+                    (lRequest ? "request" : "response") + " for " + xiSection);
+            findJsonBegin(xiSection, xiSeenRequest, xiSeenResponse);
+            return;
+          }
+
+          if ("GET".equals(lMatcher.group(1)) ||
+                   "DELETE".equals(lMatcher.group(1)) ||
+                   "204".equals(lMatcher.group(2)))
+          {
+            // Never has a body
+            findJsonBegin(xiSection,
+                          lRequest || xiSeenRequest,
+                          lResponse || xiSeenResponse);
+            return;
+          }
+          else
+          {
+            String lSection = xiSection + "." + (lRequest ? "1" : "2");
+            mProgress.log("Found start of " + lSection);
+
+            findJsonBodyStart(xiSection, lSection, xiSeenRequest, xiSeenResponse);
+            return;
+          }
+        }
+      }
+    } while ((mLine = mReader.readLine()) != null);
+
+    badEndOfFile("searching for beginning of JSON " + xiSection);
+  }
+
+  private void findJsonBodyStart(String xiSection,
+                                 String xiSubSection,
+                                 boolean xiSeenRequest,
+                                 boolean xiSeenResponse)
+      throws IOException
+  {
+    while ((mLine = mReader.readLine()) != null)
+    {
+      Matcher lMatcher = START_BODY.matcher(mLine);
+
+      if (lMatcher.matches())
+      {
+        if (lMatcher.group(1) != null)
+        {
+          findJsonBody(xiSection, xiSubSection, xiSeenRequest, xiSeenResponse);
+          return;
+        }
+        else
+        {
+          mProgress.log("*** Failed to find body start in JSON " + xiSubSection);
+          findJsonBeginHere(xiSection, xiSeenRequest, xiSeenResponse);
+          return;
+        }
+      }
+    }
+
+    badEndOfFile("searching for body start of JSON " + xiSubSection);
+  }
+
+  private void findJsonBody(String xiSection,
+                            String xiSubSection,
+                            boolean xiSeenRequest,
+                            boolean xiSeenResponse)
+                                throws IOException
+  {
+    StringBuilder lBuf = new StringBuilder();
+
+    do
+    {
+      Matcher lMatcher = END_BODY.matcher(mLine);
+
+      if (lMatcher.matches())
+      {
+        if (lMatcher.group(1) != null)
+        {
+          lBuf.append(mLine).append("\n");
+          saveJson(xiSubSection, lBuf.toString());
+          findJsonSectionStart();
+          return;
+        }
+        else
+        {
+          mProgress.log("*** Failed to find body end of JSON " + xiSubSection);
+          findJsonBeginHere(xiSection, xiSeenRequest, xiSeenResponse);
+          return;
+        }
+      }
+      else
+      {
+        lBuf.append(mLine).append("\n");
+      }
+    } while ((mLine = mReader.readLine()) != null);
+
+    badEndOfFile("searching for body end of JSON " + xiSubSection);
+  }
+
+  private void saveJson(String xiSection, String xiJson) throws IOException
+  {
+    System.out.print(xiJson);
+    mProgress.log("Done JSON " + xiSection);
+  }
+}
---
+++ testsrc/org/openmobilealliance/arc/sax2j/extract/ExtractorTest.java
@@ -0,0 +1,19 @@
+// ExtractorTest.java
+// (C) COPYRIGHT METASWITCH NETWORKS 2014
+package org.openmobilealliance.arc.sax2j.extract;
+
+import org.junit.Test;
+import org.openmobilealliance.arc.sax2j.ConsoleProgressWriter;
+
+public class ExtractorTest
+{
+
+  @Test
+  public void test() throws Exception
+  {
+    Extractor lExtractor = new Extractor("C:/code/xml2json/extracted/OMA-TS-REST_NetAPI_NMS-V1_0-20140527-D_CR123.txt");
+    lExtractor.setProgressWriter(new ConsoleProgressWriter());
+    lExtractor.extract();
+  }
+
+}
